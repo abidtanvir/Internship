aeb54153c8a84b4d3bddf1ca3ca317e1df0dc05e
S M Tanvir Abid <70322961+abidtanvir@users.noreply.github.com>
2023-12-24 20:25:13 +0100

    Add files via upload

---
+++ tcpSocketClientWithTermination.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '192.168.178.23'  # Server PC's IP address
+PORT = 6000  # same port number as the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello from Tanvir RPi!')
+    data = s.recv(1024)
+    print('Server received:', repr(data))
+    s.sendall(b"close")
