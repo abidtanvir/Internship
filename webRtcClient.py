ea49330185a6fe3a6efc8e2ed89b400749deda0d
S M Tanvir Abid <70322961+abidtanvir@users.noreply.github.com>
2024-03-10 21:36:04 +0100

    client send video via webRTC, calculate total time

---
+++ webRtcClient.py
@@ -0,0 +1,73 @@
+import asyncio
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+import aiohttp
+import time
+
+class VideoStreamTrackCustom(VideoStreamTrack):
+    def __init__(self, filename):
+        super().__init__()
+        self.filename = filename
+
+    async def recv(self):
+        with open(self.filename, 'rb') as file:
+            while True:
+                chunk = file.read(16384)
+                if not chunk:
+                    break
+                pts, time_base = await self.next_timestamp()
+                frame = {
+                    'type': 'video',
+                    'data': chunk,
+                    'timestamp': pts,
+                    'time_base': time_base,
+                }
+                yield frame
+
+async def generate_offer():
+    pc = RTCPeerConnection()
+    pc.addTransceiver("video")
+    offer = await pc.createOffer()
+    await pc.setLocalDescription(offer)
+    return pc.localDescription
+
+async def send_offer(sdp):
+    async with aiohttp.ClientSession() as session:
+        async with session.post('http://192.168.178.23:8080/offer', json={'sdp': sdp.sdp, 'type': sdp.type}) as response:
+            answer = await response.text()
+            print("Received acknowledgment:", answer)
+
+async def main():
+    start_time = time.time()
+    
+    offer = await generate_offer()
+
+    await send_offer(offer)
+
+    track = VideoStreamTrackCustom('stopSign.mp4')
+
+    pc = RTCPeerConnection()
+    pc.addTrack(track)
+
+    # Wait for ICE gathering to complete
+    await pc.createOffer()
+    print("ICE gathering complete")
+
+    # Wait for acknowledgment from the server
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://192.168.178.23:8080/ack') as response:
+            acknowledgment = await response.text()
+            print("Received acknowledgment from server:", acknowledgment)
+            
+            
+
+
+    # Calculate total time
+    print("Video sent to server Successfully")
+    # Record end time after receiving acknowledgment
+    end_time = time.time()
+    total_time = end_time - start_time
+    print("Total time:", total_time, "seconds")
+    
+    
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
